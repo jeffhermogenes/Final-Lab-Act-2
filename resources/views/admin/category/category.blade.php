6a4dd8cf5bfa2ad85fbaaf55aece61c450de536f
jeffhermogenes <115857364+jeffhermogenes@users.noreply.github.com>
2023-11-09 17:25:09 +0800

    second commit

--- resources/views/admin/category/category.blade.php
+++ resources/views/admin/category/category.blade.php
@@ -1,34 +1,33 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-            Hi {{Auth::user()->name}}
-            <b style="float:right"> Total users
-                <span class="badge text-bg-danger">{{count($users)}}</span>
-            </b>
+            All Categories
         </h2>
     </x-slot>
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            
+            {{-- <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                <x-welcome />
+            </div> --}}
             <div class="container">
+                <a href="" class="btn btn-info">Create</a>
                 <table class="table">
                     <thead>
                         <tr>
                             <th scope="col">ID</th>
-                            <th scope="col">Name</th>
-                            <th scope="col">Email</th>
+                            <th scope="col">Category Name</th>
+                            <th scope="col">User ID</th>
                             <th scope="col">Created At</th>
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach ($users as $user)
+                        @foreach ($categories as $category)
                             <tr>
-                                <th scope="row">{{ $user->id }}</th>
-                                <td>{{ $user->name }}</td>
-                                <td>{{ $user->email }}</td>
-                                <td>{{ $user->created_at }}</td>
+                                <th scope="row">{{ $category->id }}</th>
+                                <td>{{ $category->cat_name }}</td>
+                                <td>{{ $category->user_id }}</td>
+                                <td>{{ $category->created_at }}</td>
                             </tr>
                         @endforeach
                     </tbody>
@@ -37,3 +36,4 @@
         </div>
     </div>
 </x-app-layout>
+
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,8 +10,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
 |
 */
 
@@ -20,9 +22,16 @@ Route::get('/', function () {
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
-    'verified'
+    'verified',
 ])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        $users = User::all();
+        return view('dashboard', compact('users'));
     })->name('dashboard');
 });
+
+//Route::get('/category', function () {
+ //   return view('admin.category.category');
+//})->name('AllCat');
+
+Route::get('/all/actegory', [CategoryController ::class,'index'])->name('AllCat');
